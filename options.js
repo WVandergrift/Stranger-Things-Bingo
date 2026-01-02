9290abe45a9c47e4742c770db182218c76e22754
Will Vandergrift <wvandergrift@poolplayers.com>
2026-01-02 17:38:16 -0600

    Centralize bingo options

---
+++ options.js
@@ -0,0 +1,30 @@
+export const BINGO_OPTIONS = [
+    "Eleven",
+    "Will the Wise",
+    "Mike Wheeler",
+    "Dustin",
+    "Max Mayfield",
+    "Steve",
+    "Hopper",
+    "Joyce Byers",
+    "Hawkins",
+    "Hawkins Lab",
+    "The Upside Down",
+    "Starcourt Mall",
+    "Scoops Ahoy",
+    "The Arcade",
+    "Vecna's Clock",
+    "Walkie-Talkie",
+    "Christmas Lights",
+    "Bicycle",
+    "Waffles",
+    "The Squawk",
+    "Radio",
+    "Baseball Bat",
+    "Vecna",
+    "Portal"
+];
+
+if (typeof window !== "undefined") {
+    window.BINGO_OPTIONS = BINGO_OPTIONS;
+}
